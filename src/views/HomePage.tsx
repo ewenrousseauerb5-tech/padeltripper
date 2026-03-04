58c1be23e99b5655e6ed07ca3d3b9909638fef3f
TuUsername <tuemail@gmail.com>
2026-03-04 22:49:42 +0800

    Nudge Ollie photo crop slightly higher

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -67,7 +67,7 @@ export default function HomePage() {
                 <img
                   src="/images/ollie.jpg"
                   alt="Ollie, co-founder of Padel Tripper, on court in Alicante"
-                  className="w-full h-[520px] object-cover object-[center_26%]"
+                  className="w-full h-[520px] object-cover object-[center_24%]"
                 />
               </div>
             </motion.div>
