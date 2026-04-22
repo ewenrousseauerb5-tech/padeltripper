e2c0b318a4f10518988b20ece5f247b878630bdf
TuUsername <tuemail@gmail.com>
2026-04-22 10:57:05 +0800

    Use different image for May 12 event card

--- src/data/events.ts
+++ src/data/events.ts
@@ -93,8 +93,8 @@ export const ALL_EVENTS: PadelEvent[] = [
     max: 16,
     location: 'Alicante, Spain',
     hotel: '4* Hotel Alicante Golf',
-    image: '/images/group-bela-court.jpg',
-    imagePosition: 'slightLower',
+    image: '/images/group-photo.jpg',
+    imagePosition: 'lower',
   },
   {
     id: 70,
